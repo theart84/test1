1e226a7a4598e4b5072f13aeb8a27bc12ce9c6ea
Julia Fokanova <oncologmma@mail.ru>
2024-11-09 19:31:18 +0300

    changes for redeployment

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,14 +3,15 @@ import './notFoundPage.css';
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
+  const getBack = () => navigate(-1);
 
   return (
     <div className="notFound">
       <h1 className="notFound__title">404</h1>
       <p className="notFound__text">Запрашиваемая страница не найдена...</p>
       {/* <Link className="notFound__btn" to="/RA_router-menu">На главную</Link> */}
-      {/* FIXME: можно ли реализовать возврат на шаг назад, не вызывая перезагрузки страницы ??? */}
-      <button className="notFound__btn" onClick={() => navigate(-1)}>Вернуться назад</button>
+      {/* FIXME: как сделать возврат на шаг назад без Link, без перезагрузки страницы ? */}
+      <button className="notFound__btn" onClick={getBack}>Вернуться назад</button>
     </div>
   );
 };
